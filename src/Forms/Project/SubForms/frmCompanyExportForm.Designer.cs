80a43ac6f8f9ed9c12820e457290109e317ed731
Oliver Kind <github@oliver-kind.de>
2024-12-28 11:14:58 +0100

    Initial commit

---
+++ src/Forms/Project/SubForms/frmCompanyExportForm.Designer.cs
@@ -0,0 +1,320 @@
+﻿namespace OLKI.Programme.BeOrSy.src.Forms.Project.SubForms
+{
+    partial class CompanyExportForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CompanyExportForm));
+            this.rabNewlineSpace = new System.Windows.Forms.RadioButton();
+            this.rabNewlineLF = new System.Windows.Forms.RadioButton();
+            this.rabNewlineCRLF = new System.Windows.Forms.RadioButton();
+            this.rabNewlineBR = new System.Windows.Forms.RadioButton();
+            this.rabNewlineCustom = new System.Windows.Forms.RadioButton();
+            this.grbHandleNewline = new System.Windows.Forms.GroupBox();
+            this.txtNewlineCustom = new System.Windows.Forms.TextBox();
+            this.grbHandleSeperator = new System.Windows.Forms.GroupBox();
+            this.txtSeperatorCustom = new System.Windows.Forms.TextBox();
+            this.rabSeperatorComma = new System.Windows.Forms.RadioButton();
+            this.rabSeperatorCustom = new System.Windows.Forms.RadioButton();
+            this.rabSeperatorSemicolon = new System.Windows.Forms.RadioButton();
+            this.rabSeperatorTabulator = new System.Windows.Forms.RadioButton();
+            this.grbExportScope = new System.Windows.Forms.GroupBox();
+            this.rabExportAll = new System.Windows.Forms.RadioButton();
+            this.rabExportSel = new System.Windows.Forms.RadioButton();
+            this.rabExportFil = new System.Windows.Forms.RadioButton();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.grbHandleNewline.SuspendLayout();
+            this.grbHandleSeperator.SuspendLayout();
+            this.grbExportScope.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // rabNewlineSpace
+            // 
+            this.rabNewlineSpace.AutoSize = true;
+            this.rabNewlineSpace.Checked = true;
+            this.rabNewlineSpace.Location = new System.Drawing.Point(6, 19);
+            this.rabNewlineSpace.Name = "rabNewlineSpace";
+            this.rabNewlineSpace.Size = new System.Drawing.Size(83, 17);
+            this.rabNewlineSpace.TabIndex = 0;
+            this.rabNewlineSpace.TabStop = true;
+            this.rabNewlineSpace.Text = "Leerzeichen";
+            this.rabNewlineSpace.UseVisualStyleBackColor = true;
+            // 
+            // rabNewlineLF
+            // 
+            this.rabNewlineLF.AutoSize = true;
+            this.rabNewlineLF.Location = new System.Drawing.Point(6, 42);
+            this.rabNewlineLF.Name = "rabNewlineLF";
+            this.rabNewlineLF.Size = new System.Drawing.Size(107, 17);
+            this.rabNewlineLF.TabIndex = 1;
+            this.rabNewlineLF.Text = "\\n - Zeichenfolge";
+            this.rabNewlineLF.UseVisualStyleBackColor = true;
+            // 
+            // rabNewlineCRLF
+            // 
+            this.rabNewlineCRLF.AutoSize = true;
+            this.rabNewlineCRLF.Location = new System.Drawing.Point(6, 65);
+            this.rabNewlineCRLF.Name = "rabNewlineCRLF";
+            this.rabNewlineCRLF.Size = new System.Drawing.Size(115, 17);
+            this.rabNewlineCRLF.TabIndex = 2;
+            this.rabNewlineCRLF.Text = "\\r\\n - Zeichenfolge";
+            this.rabNewlineCRLF.UseVisualStyleBackColor = true;
+            // 
+            // rabNewlineBR
+            // 
+            this.rabNewlineBR.AutoSize = true;
+            this.rabNewlineBR.Location = new System.Drawing.Point(6, 88);
+            this.rabNewlineBR.Name = "rabNewlineBR";
+            this.rabNewlineBR.Size = new System.Drawing.Size(125, 17);
+            this.rabNewlineBR.TabIndex = 3;
+            this.rabNewlineBR.Text = "<br /> - Zeichenfolge";
+            this.rabNewlineBR.UseVisualStyleBackColor = true;
+            // 
+            // rabNewlineCustom
+            // 
+            this.rabNewlineCustom.AutoSize = true;
+            this.rabNewlineCustom.Location = new System.Drawing.Point(6, 111);
+            this.rabNewlineCustom.Name = "rabNewlineCustom";
+            this.rabNewlineCustom.Size = new System.Drawing.Size(107, 17);
+            this.rabNewlineCustom.TabIndex = 4;
+            this.rabNewlineCustom.Text = "Benutzerdefiniert:";
+            this.rabNewlineCustom.UseVisualStyleBackColor = true;
+            // 
+            // grbHandleNewline
+            // 
+            this.grbHandleNewline.Controls.Add(this.txtNewlineCustom);
+            this.grbHandleNewline.Controls.Add(this.rabNewlineSpace);
+            this.grbHandleNewline.Controls.Add(this.rabNewlineCustom);
+            this.grbHandleNewline.Controls.Add(this.rabNewlineLF);
+            this.grbHandleNewline.Controls.Add(this.rabNewlineBR);
+            this.grbHandleNewline.Controls.Add(this.rabNewlineCRLF);
+            this.grbHandleNewline.Location = new System.Drawing.Point(12, 225);
+            this.grbHandleNewline.Name = "grbHandleNewline";
+            this.grbHandleNewline.Size = new System.Drawing.Size(300, 137);
+            this.grbHandleNewline.TabIndex = 2;
+            this.grbHandleNewline.TabStop = false;
+            this.grbHandleNewline.Text = "Zeilenumbruch ersetzen durch:";
+            // 
+            // txtNewlineCustom
+            // 
+            this.txtNewlineCustom.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.txtNewlineCustom.Location = new System.Drawing.Point(119, 111);
+            this.txtNewlineCustom.MaxLength = 20;
+            this.txtNewlineCustom.Name = "txtNewlineCustom";
+            this.txtNewlineCustom.Size = new System.Drawing.Size(175, 20);
+            this.txtNewlineCustom.TabIndex = 5;
+            // 
+            // grbHandleSeperator
+            // 
+            this.grbHandleSeperator.Controls.Add(this.txtSeperatorCustom);
+            this.grbHandleSeperator.Controls.Add(this.rabSeperatorComma);
+            this.grbHandleSeperator.Controls.Add(this.rabSeperatorCustom);
+            this.grbHandleSeperator.Controls.Add(this.rabSeperatorSemicolon);
+            this.grbHandleSeperator.Controls.Add(this.rabSeperatorTabulator);
+            this.grbHandleSeperator.Location = new System.Drawing.Point(12, 106);
+            this.grbHandleSeperator.Name = "grbHandleSeperator";
+            this.grbHandleSeperator.Size = new System.Drawing.Size(300, 113);
+            this.grbHandleSeperator.TabIndex = 1;
+            this.grbHandleSeperator.TabStop = false;
+            this.grbHandleSeperator.Text = "Trennzeichen:";
+            // 
+            // txtSeperatorCustom
+            // 
+            this.txtSeperatorCustom.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.txtSeperatorCustom.Location = new System.Drawing.Point(119, 87);
+            this.txtSeperatorCustom.MaxLength = 20;
+            this.txtSeperatorCustom.Name = "txtSeperatorCustom";
+            this.txtSeperatorCustom.Size = new System.Drawing.Size(175, 20);
+            this.txtSeperatorCustom.TabIndex = 4;
+            // 
+            // rabSeperatorComma
+            // 
+            this.rabSeperatorComma.AutoSize = true;
+            this.rabSeperatorComma.Location = new System.Drawing.Point(6, 19);
+            this.rabSeperatorComma.Name = "rabSeperatorComma";
+            this.rabSeperatorComma.Size = new System.Drawing.Size(60, 17);
+            this.rabSeperatorComma.TabIndex = 0;
+            this.rabSeperatorComma.Text = "Komma";
+            this.rabSeperatorComma.UseVisualStyleBackColor = true;
+            // 
+            // rabSeperatorCustom
+            // 
+            this.rabSeperatorCustom.AutoSize = true;
+            this.rabSeperatorCustom.Location = new System.Drawing.Point(6, 88);
+            this.rabSeperatorCustom.Name = "rabSeperatorCustom";
+            this.rabSeperatorCustom.Size = new System.Drawing.Size(107, 17);
+            this.rabSeperatorCustom.TabIndex = 3;
+            this.rabSeperatorCustom.Text = "Benutzerdefiniert:";
+            this.rabSeperatorCustom.UseVisualStyleBackColor = true;
+            // 
+            // rabSeperatorSemicolon
+            // 
+            this.rabSeperatorSemicolon.AutoSize = true;
+            this.rabSeperatorSemicolon.Checked = true;
+            this.rabSeperatorSemicolon.Location = new System.Drawing.Point(6, 42);
+            this.rabSeperatorSemicolon.Name = "rabSeperatorSemicolon";
+            this.rabSeperatorSemicolon.Size = new System.Drawing.Size(74, 17);
+            this.rabSeperatorSemicolon.TabIndex = 1;
+            this.rabSeperatorSemicolon.TabStop = true;
+            this.rabSeperatorSemicolon.Text = "Semikolon";
+            this.rabSeperatorSemicolon.UseVisualStyleBackColor = true;
+            // 
+            // rabSeperatorTabulator
+            // 
+            this.rabSeperatorTabulator.AutoSize = true;
+            this.rabSeperatorTabulator.Location = new System.Drawing.Point(6, 65);
+            this.rabSeperatorTabulator.Name = "rabSeperatorTabulator";
+            this.rabSeperatorTabulator.Size = new System.Drawing.Size(70, 17);
+            this.rabSeperatorTabulator.TabIndex = 2;
+            this.rabSeperatorTabulator.Text = "Tabulator";
+            this.rabSeperatorTabulator.UseVisualStyleBackColor = false;
+            // 
+            // grbExportScope
+            // 
+            this.grbExportScope.Controls.Add(this.rabExportAll);
+            this.grbExportScope.Controls.Add(this.rabExportSel);
+            this.grbExportScope.Controls.Add(this.rabExportFil);
+            this.grbExportScope.Location = new System.Drawing.Point(12, 12);
+            this.grbExportScope.Name = "grbExportScope";
+            this.grbExportScope.Size = new System.Drawing.Size(300, 88);
+            this.grbExportScope.TabIndex = 0;
+            this.grbExportScope.TabStop = false;
+            this.grbExportScope.Text = "Exportieren:";
+            // 
+            // rabExportAll
+            // 
+            this.rabExportAll.AutoSize = true;
+            this.rabExportAll.Checked = true;
+            this.rabExportAll.Location = new System.Drawing.Point(6, 19);
+            this.rabExportAll.Name = "rabExportAll";
+            this.rabExportAll.Size = new System.Drawing.Size(76, 17);
+            this.rabExportAll.TabIndex = 0;
+            this.rabExportAll.TabStop = true;
+            this.rabExportAll.Text = "Alle Firmen";
+            this.rabExportAll.UseVisualStyleBackColor = true;
+            // 
+            // rabExportSel
+            // 
+            this.rabExportSel.AutoSize = true;
+            this.rabExportSel.Location = new System.Drawing.Point(6, 42);
+            this.rabExportSel.Name = "rabExportSel";
+            this.rabExportSel.Size = new System.Drawing.Size(120, 17);
+            this.rabExportSel.TabIndex = 1;
+            this.rabExportSel.Text = "Ausgewählte Firmen";
+            this.rabExportSel.UseVisualStyleBackColor = true;
+            // 
+            // rabExportFil
+            // 
+            this.rabExportFil.AutoSize = true;
+            this.rabExportFil.Location = new System.Drawing.Point(6, 65);
+            this.rabExportFil.Name = "rabExportFil";
+            this.rabExportFil.Size = new System.Drawing.Size(101, 17);
+            this.rabExportFil.TabIndex = 2;
+            this.rabExportFil.Text = "Gefilterte Firmen";
+            this.rabExportFil.UseVisualStyleBackColor = true;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancel.Image = global::OLKI.Programme.BeOrSy.Properties.Resources.Cancel;
+            this.btnCancel.Location = new System.Drawing.Point(202, 368);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(110, 23);
+            this.btnCancel.TabIndex = 4;
+            this.btnCancel.Text = "&Abbrechen";
+            this.btnCancel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnCancel.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // btnOk
+            // 
+            this.btnOk.Image = global::OLKI.Programme.BeOrSy.Properties.Resources.Ok;
+            this.btnOk.Location = new System.Drawing.Point(12, 368);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(110, 23);
+            this.btnOk.TabIndex = 3;
+            this.btnOk.Text = "&Ok";
+            this.btnOk.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnOk.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnOk.UseVisualStyleBackColor = true;
+            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+            // 
+            // CompanyExportForm
+            // 
+            this.AcceptButton = this.btnOk;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnCancel;
+            this.ClientSize = new System.Drawing.Size(324, 403);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnOk);
+            this.Controls.Add(this.grbExportScope);
+            this.Controls.Add(this.grbHandleSeperator);
+            this.Controls.Add(this.grbHandleNewline);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "CompanyExportForm";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Firmen exportieren";
+            this.grbHandleNewline.ResumeLayout(false);
+            this.grbHandleNewline.PerformLayout();
+            this.grbHandleSeperator.ResumeLayout(false);
+            this.grbHandleSeperator.PerformLayout();
+            this.grbExportScope.ResumeLayout(false);
+            this.grbExportScope.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton rabNewlineSpace;
+        private System.Windows.Forms.RadioButton rabNewlineLF;
+        private System.Windows.Forms.RadioButton rabNewlineCRLF;
+        private System.Windows.Forms.RadioButton rabNewlineBR;
+        private System.Windows.Forms.RadioButton rabNewlineCustom;
+        private System.Windows.Forms.GroupBox grbHandleNewline;
+        private System.Windows.Forms.TextBox txtNewlineCustom;
+        private System.Windows.Forms.GroupBox grbHandleSeperator;
+        private System.Windows.Forms.RadioButton rabSeperatorComma;
+        private System.Windows.Forms.RadioButton rabSeperatorCustom;
+        private System.Windows.Forms.RadioButton rabSeperatorSemicolon;
+        private System.Windows.Forms.RadioButton rabSeperatorTabulator;
+        private System.Windows.Forms.TextBox txtSeperatorCustom;
+        private System.Windows.Forms.GroupBox grbExportScope;
+        private System.Windows.Forms.RadioButton rabExportAll;
+        private System.Windows.Forms.RadioButton rabExportSel;
+        private System.Windows.Forms.RadioButton rabExportFil;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnOk;
+    }
+}
---
+++ src/Forms/Project/SubForms/frmCompanyExportForm.cs
@@ -0,0 +1,260 @@
+﻿/*
+ * BeOrSy - Bewerbungsorganisationssystem
+ * 
+ * Copyright:   Oliver Kind - 2024
+ * License:     LGPL
+ * 
+ * Desctiption:
+ * Form to export Companies
+ * 
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the LGPL General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * LGPL General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not check the GitHub-Repository.
+ * 
+ * */
+
+using OLKI.Programme.BeOrSy.Properties;
+using OLKI.Programme.BeOrSy.src.Project;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace OLKI.Programme.BeOrSy.src.Forms.Project.SubForms
+{
+    /// <summary>
+    /// Form to export Companies
+    /// </summary>
+    public partial class CompanyExportForm : Form
+    {
+        #region Fields
+        /// <summary>
+        /// Items in Company ListView
+        /// </summary>
+        private readonly ListView.ListViewItemCollection _companyListViewItems;
+
+        /// <summary>
+        /// Selected Items in Company ListView
+        /// </summary>
+        private readonly ListView.SelectedListViewItemCollection _companyListViewItemsSelected;
+
+        /// <summary>
+        /// Main Project
+        /// </summary>
+        private readonly src.Project.Project _project;
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Initial a new Form to export Companies
+        /// </summary>
+        /// <param name="companyListViewItems">Items in Company ListView</param>
+        /// <param name="companyListViewItemsSelected">Selected Items in Company ListView</param>
+        /// <param name="project">Main Project</param>
+        public CompanyExportForm(ListView.ListViewItemCollection companyListViewItems, ListView.SelectedListViewItemCollection companyListViewItemsSelected, src.Project.Project project)
+        {
+            InitializeComponent();
+
+            this._companyListViewItems = companyListViewItems;
+            this._companyListViewItemsSelected = companyListViewItemsSelected;
+            this._project = project;
+
+            this.rabExportAll.Enabled = this._project.Companies.Count > 0;
+            this.rabExportFil.Enabled = this._companyListViewItems.Count > 0;
+            this.rabExportSel.Enabled = this._companyListViewItemsSelected.Count > 0;
+        }
+
+        /// <summary>
+        /// Export Companies to target File
+        /// </summary>
+        /// <param name="targetFile">File to export to</param>
+        /// <param name="companies">Companies to export</param>
+        /// <param name="newline">Replacement for NewLine</param>
+        /// <param name="seperator">Seperator for Columns</param>
+        private void ExportCompanies(string targetFile, List<CompanyItem> companies, string newline, string seperator)
+        {
+            string Template = Resources.CompanyTemplate;
+            Template = Template.Replace(";", seperator);
+
+            StringBuilder DataSring = new StringBuilder();
+            DataSring.AppendLine(Template);
+
+            StringBuilder DataLineBuilder = new StringBuilder();
+            List<string> CompanyData = new List<string>();
+            foreach (CompanyItem Item in companies.OrderBy(o => o.TitleNoText))
+            {
+                CompanyData.Clear();
+
+                CompanyData.Add(Item.TitleNoText);
+                CompanyData.Add(Item.Trade);
+                CompanyData.Add(Item.Terminated.ToString());
+                CompanyData.Add(Item.AdressStreet.Replace("\r\n", newline));
+                CompanyData.Add(Item.AdressPostcode);
+                CompanyData.Add(Item.AdressCity);
+                CompanyData.Add(Item.AdressState);
+                CompanyData.Add(Item.AdressCountry);
+                CompanyData.Add(Item.ContactTelephone);
+                CompanyData.Add(Item.ContactFax);
+                CompanyData.Add(Item.ContactEMail);
+                CompanyData.Add(Item.ContactHomepage);
+                CompanyData.Add(Item.ContactCareerPage);
+                CompanyData.Add(Item.Comment.Replace("\r\n", newline));
+
+                DataSring.AppendLine(this.GetDataLine(DataLineBuilder, CompanyData, newline, seperator));
+            }
+            using (StreamWriter outputFile = new StreamWriter(targetFile, false))
+            {
+                outputFile.WriteLine(DataSring.ToString());
+            }
+        }
+
+        /// <summary>
+        /// Get the data Line
+        /// </summary>
+        /// <param name="dataLineBuilder">StringBuilder to create a data line</param>
+        /// <param name="companyData">Data of the Company, to create a data line from</param>
+        /// <param name="newline">Newline replacement</param>
+        /// <param name="seperator">Column seperator</param>
+        /// <returns>Data line for CSV-File</returns>
+        string GetDataLine(StringBuilder dataLineBuilder, List<string> companyData, string newline, string seperator)
+        {
+            dataLineBuilder.Clear();
+            for (int i = 0; i < companyData.Count; i++)
+            {
+                if (i > 0) dataLineBuilder.Append(seperator);
+
+                string CompanyData = companyData[i];
+                CompanyData = CompanyData.Replace("\r\n", newline);
+                CompanyData = CompanyData.Replace("\"", "\"\"");
+                if (CompanyData.Contains(seperator))
+                {
+                    dataLineBuilder.Append("\"");
+                    dataLineBuilder.Append(CompanyData);
+                    dataLineBuilder.Append("\"");
+                }
+                else
+                {
+                    dataLineBuilder.Append(CompanyData);
+                }
+            }
+
+            return dataLineBuilder.ToString();
+        }
+
+        /// <summary>
+        /// Get a list with Companies to export
+        /// </summary>
+        /// <returns>A list with Companies to export</returns>
+        private List<CompanyItem> GetExportCompanies()
+        {
+            List<CompanyItem> ExportList = new List<CompanyItem>();
+            List<int> FilterIdSet = new List<int>();
+
+            if (this.rabExportFil.Checked && this._companyListViewItems != null)
+            {
+                foreach (ListViewItem Item in this._companyListViewItems)
+                {
+                    FilterIdSet.Add((int)Item.Tag);
+                }
+            }
+
+            if (this.rabExportSel.Checked && this._companyListViewItemsSelected != null)
+            {
+                foreach (ListViewItem Item in this._companyListViewItemsSelected)
+                {
+                    FilterIdSet.Add((int)Item.Tag);
+                }
+            }
+
+            foreach (CompanyItem Item in this._project.Companies.Values)
+            {
+                if (this.rabExportAll.Checked || FilterIdSet.Contains(Item.Id))
+                {
+                    ExportList.Add(Item);
+                }
+            }
+
+            return ExportList;
+        }
+
+        /// <summary>
+        /// Get if there are Companies to export
+        /// </summary>
+        /// <returns>True if there are any Companoes to export</returns>
+        private bool HasExportData()
+        {
+            if (this.rabExportAll.Checked && this._project.Companies != null && this._project.Companies.Count > 0) return true;
+            if (this.rabExportFil.Checked && this._companyListViewItems != null && this._companyListViewItems.Count > 0) return true;
+            if (this.rabExportSel.Checked && this._companyListViewItemsSelected != null && this._companyListViewItemsSelected.Count > 0) return true;
+
+            return false;
+        }
+
+        #region Form Events
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            if (!this.HasExportData())
+            {
+                MessageBox.Show(this, Stringtable._0x0044m, Stringtable._0x0044m, MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            string TargetPath = string.Empty;
+            SaveFileDialog SaveFileDialog = new SaveFileDialog
+            {
+                DefaultExt = Settings_AppConst.Default.CompanyExportFile_DefaultExtension,
+                Filter = Settings_AppConst.Default.CompanyExportFile_FilterList,
+                FilterIndex = Settings_AppConst.Default.CompanyExportFile_FilterIndex,
+                InitialDirectory = Properties.Settings.Default.DirectoryFile_DefaultPath,
+                SupportMultiDottedExtensions = true
+            };
+            if (SaveFileDialog.ShowDialog(this) != DialogResult.OK) return;
+            TargetPath = SaveFileDialog.FileName;
+
+            List<CompanyItem> Companies = new List<CompanyItem>();
+            Companies = GetExportCompanies();
+
+            string Newline = "";
+            if (this.rabNewlineSpace.Checked) Newline = " ";
+            if (this.rabNewlineLF.Checked) Newline = @"\n";
+            if (this.rabNewlineCRLF.Checked) Newline = @"\r\n";
+            if (this.rabNewlineBR.Checked) Newline = @"<br />";
+            if (this.rabNewlineCustom.Checked) Newline = this.txtNewlineCustom.Text;
+
+            string Seperator = "";
+            if (this.rabSeperatorComma.Checked) Seperator = ",";
+            if (this.rabSeperatorSemicolon.Checked) Seperator = ";";
+            if (this.rabSeperatorTabulator.Checked) Seperator = "\t";
+            if (this.rabSeperatorCustom.Checked) Seperator = this.txtSeperatorCustom.Text;
+
+            try
+            {
+                this.ExportCompanies(TargetPath, Companies, Newline, Seperator);
+                this.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(this, string.Format(Stringtable._0x0045m, new object[] { ex.Message }), Stringtable._0x0045c, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        #endregion
+        #endregion
+    }
+}
