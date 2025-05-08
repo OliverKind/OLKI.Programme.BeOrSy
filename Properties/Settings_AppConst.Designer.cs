aa61cb67fcb9b20eb74e532055a346fa331258f2
Oliver Kind <github@oliver-kind.de>
2025-05-08 11:27:55 +0200

    Add: Export Companies to XML

--- Properties/Settings_AppConst.Designer.cs
+++ Properties/Settings_AppConst.Designer.cs
@@ -12,7 +12,7 @@ namespace OLKI.Programme.BeOrSy.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.12.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.13.0.0")]
     internal sealed partial class Settings_AppConst : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings_AppConst defaultInstance = ((Settings_AppConst)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings_AppConst())));
@@ -400,7 +400,7 @@ namespace OLKI.Programme.BeOrSy.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("CSV-Datei (*.csv)|*.csv")]
+        [global::System.Configuration.DefaultSettingValueAttribute("CSV-Datei (*.csv)|*.csv|XML-Datei (*.xml)|*.xml")]
         public string CompanyExportFile_FilterList {
             get {
                 return ((string)(this["CompanyExportFile_FilterList"]));
