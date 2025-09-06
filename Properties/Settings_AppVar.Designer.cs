e256ef49e208578a12ed297fbc1de92219408139
Oliver Kind <github@oliver-kind.de>
2025-09-06 19:10:09 +0200

    Set required .NET-Framework to Version 4.8.1

--- Properties/Settings_AppVar.Designer.cs
+++ Properties/Settings_AppVar.Designer.cs
@@ -12,7 +12,7 @@ namespace OLKI.Programme.BeOrSy.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.11.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.14.0.0")]
     internal sealed partial class Settings_AppVar : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings_AppVar defaultInstance = ((Settings_AppVar)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings_AppVar())));
